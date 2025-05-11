c44a158d9f9c220550b95f5e4c8620531815f695
Markbull <giupy997@gmail.com>
2025-05-12 00:55:24 +0200

    Initial commit: fix backend for Casino of Meme

--- src/components/RewardsDashboard.jsx
+++ src/components/RewardsDashboard.jsx
@@ -4801,7 +4801,7 @@ const spinWheel = async (event) => {
           <svg width="16" height="16" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="7" cy="7" r="7" fill="#FF0000" />
           </svg>
-          hello@casinoofmeme.com
+          casinofmeme@gmail.com
         </div>
         <div className="footer-info-item flex items-center gap-3">
           <svg width="16" height="16" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
